8b8375b97b506367de92fc710012fe616338ebcc
Macca <yamaraurquzam@gmail.com>
2025-12-14 11:50:00 -0400

    Create excercicío-estruturas-aninhadas.py

---
+++ excercicío-estruturas-aninhadas.py
@@ -0,0 +1,20 @@
+#estruturas aninhadas:
+categoria = int(input("qual a sua categoria: "))
+if categoria == 1:
+    preço = 10
+else:
+    if categoria == 2:
+        preço = 18
+    else:
+        if categoria == 3:
+            preço = 23
+        else:
+            if categoria == 4:
+                preço = 26
+            else:
+                if categoria == 5:
+                    preço = 31
+                else:
+                    print("categoria invalida, digite um valor de 1 a 5")
+                    preço = 0
+print("O valor da sua categoria é R$%5.2f" % preço)
