16bc6dbd030e004db5164ff98f6dfd88515393c3
Macca <yamaraurquzam@gmail.com>
2025-12-14 11:26:51 -0400

    Create Exercicío-condicões1.py

---
+++ Exercicío-condicões1.py
@@ -0,0 +1,6 @@
+#atividade, carro é velho ou novo:
+idade_do_carro = int(input("qual a idade do seu carro: "))
+if idade_do_carro <= 3:
+   print("seu carro é novo")
+else:
+    print("seu carro é velho")
